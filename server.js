e18702f11de582f797e860cb6d350e388088d883
Ankur singh <ankur@rudrainnovative.in>
2023-02-27 18:56:45 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+import { ApolloServer, gql } from "apollo-server";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+
+const typeDefs = gql`
+  type Query {
+    greet: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greet: () => {
+      return "Hello world";
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+
+server.listen().then(({ url }) => {
+  console.log(`server listening at ${url}`);
+});

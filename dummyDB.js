50c3d5413324740a197ad237c8f98bb164056a30
Ankur singh <ankur@rudrainnovative.in>
2023-02-28 18:54:24 +0530

    second day learning

---
+++ dummyDB.js
@@ -0,0 +1,34 @@
+export const users = [
+    {
+        id:"456257",
+        firstName: "john",
+        lastName: "singh",
+        email: "john@gmail.com",
+        password: "123456"
+    },
+    {
+        id:"23426",
+        firstName: "smith",
+        lastName: "singh",
+        email: "smith@gmail.com",
+        password: "abcdef"
+    }
+]
+
+export const products = [
+    {
+        name:"iPhone",
+        price:"5678",
+        by: "456257"
+    },
+    {
+        name:"Shirts",
+        price:"654788",
+        by: "456257"
+    },
+    {
+        name:"Tank",
+        price:"7657845",
+        by: "23426"
+    }
+]
--- server.js
+++ server.js
@@ -1,25 +1,49 @@
 import { ApolloServer, gql } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { users, products } from "./dummyDB.js";
 
 const typeDefs = gql`
   type Query {
-    greet: String
+    users: [User]
+    user(id:ID!): User
+    products: [Product]
+    myproducts(by:ID!): [Product]
   }
+
+type User {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
+    products: [Product]
+}
+
+  type Product {
+    name: String
+    price: String
+    by: ID!
+  }
+
 `;
 
 const resolvers = {
   Query: {
-    greet: () => {
-      return "Hello world";
-    },
+    users: () => users,
+    user:(parent, {id} )=> users.find(user=> user.id == id),
+    products: ()=> products,
+    myproducts:(_, {by}) => products.filter(products => products.by == by)
   },
+  User: {
+    products: (ur)=> products.filter(products=> products.by == ur.id) 
+  }
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
-});
+}); 
 
 server.listen().then(({ url }) => {
   console.log(`server listening at ${url}`);
